14a01bfa233c1517fd64c8491c12f4a03d1776ad
KunalDuran <kunalduran13@gmail.com>
2020-11-04 12:45:18 +0530

    businessed

--- duranzwebsite/duranz/routes.py
+++ duranzwebsite/duranz/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for, request, flash
 from duranz.forms import ProjectRequestForm
 from duranz import app
+from duranz.send_mail import send_it
 
 
 @app.route("/")
@@ -22,7 +23,18 @@ def services():
 def query_form():
     form = ProjectRequestForm()
     if form.validate_on_submit():
-        pass
+        subject = form.project.data
+        body = '  =====   '.join([ form.name.data, form.email.data, form.project.data, form.detail.data])
+        send_it(subject, body)
+        mailed(form.email.data)
+        return redirect(url_for('home'))
+    else:
+        print("something went wrong")
     return render_template('query_form.html', form=form, title='Request Service')
 
+
+def mailed(recipients):
+    msg = Message('Repair Request Accepted', recipients=[recipients])
+    msg.body = ''' Thanks for using our service. Keep in touch with us on our whatsapp number '''
+    mail.send(msg)
 
